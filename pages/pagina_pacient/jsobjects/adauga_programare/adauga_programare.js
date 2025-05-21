18bf2f2301b1ada6de50a3b544d945bc543976a8
iulliiia <iulliiia>
2025-05-21 08:55:16 +0000

    am modificat pagina receptioner si pagina pacient

--- pages/pagina_pacient/jsobjects/adauga_programare/adauga_programare.js
+++ pages/pagina_pacient/jsobjects/adauga_programare/adauga_programare.js
@@ -1,17 +1,6 @@
 export default {
   adaugaProgramare() {
-    const dataRaw = Form_Add_Programare.formData.data;
-    const oraRaw = Form_Add_Programare.formData.ora;
-
-    // Validare date
-    const isValidDate = moment(dataRaw, "YYYY-MM-DD", true).isValid();
-    const isValidOra = oraRaw && oraRaw.match(/^([0-1]\d|2[0-3]):(00|30)$/);
-
-    if (!isValidDate || !isValidOra) {
-      showAlert("Data sau ora selectată este invalidă!", "error");
-      return;
-    }
-
+  
     // Verificăm dacă există deja programare
     query_verifica_programarea.run(() => {
       const exista = Number(query_verifica_programarea.data[0].count);
